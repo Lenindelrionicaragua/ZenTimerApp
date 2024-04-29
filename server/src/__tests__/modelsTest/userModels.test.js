c5e95dcd0ca8e28c5000e74ed4cb0f81c38ce461
lenin del rio <lenindelrionicaragua@gmail.com>
2024-04-29 17:22:49 +0200

    testing validarUser function

--- server/src/__tests__/modelsTest/userModels.test.js
+++ server/src/__tests__/modelsTest/userModels.test.js
@@ -1,25 +1,90 @@
-import { validateUser } from "../../models/userModels.js";
+import { validateUser } from "../../models/userModels";
 
 describe("validateUser function", () => {
-  test("should return an empty array if all required fields are provided", () => {
-    const user = {
-      name: "John Doe",
-      email: "john@example.com",
-      password: "Password123",
-      dateOfBirth: "1990-01-01",
-    };
+//   test("should return an empty array if all required fields are provided", () => {
+//     const user = {
+//       name: "John Doe",
+//       email: "john@example.com",
+//       password: "Password123",
+//       dateOfBirth: "1990-01-01",
+//     };
 
-    const errors = validateUser(user);
+//     const errors = validateUser(user);
     
-    expect(errors).toHaveLength(0);
-  });
+//     expect(errors).toHaveLength(0);
+//   });
 
-  test("should return an array with error messages if required fields are missing", () => {
-    const user = {};
+//   test("should return an error messages if the name is null", () => {
+//     const user = {
+//               name: null,
+//               email: "john@example.com",
+//               password: "Password123",
+//               dateOfBirth: "1990-01-01",
+//             };
 
-    const errors = validateUser(user);
+//     const errors = validateUser(user);
     
-    expect(errors).toHaveLength(4);
-    expect(errors).toContain("name is a required field");
-    expect(errors).toContain("email is a required field");
-    expect(errors).toContain("passwo
+//     expect(errors).toHaveLength(1);
+//     expect(errors).toContainEqual("name is a required field");
+//   });
+
+//   test("should return an error messages if the name is and empty field", () => {
+//     const user = {
+//               name: "",
+//               email: "john@example.com",
+//               password: "Password123",
+//               dateOfBirth: "1990-01-01",
+//             };
+
+//     const errors = validateUser(user);
+   
+//     expect(errors).toHaveLength(2);
+//     expect(errors).toContainEqual("name is a required field");
+//     expect(errors).toContainEqual("name can only contain letters and numbers, with no spaces");
+//   });
+
+//   test("Should return an error message if the name contains invalid symbols", () => {
+//     const user = {
+//               name: "Anne!$",
+//               email: "john@example.com",
+//               password: "Password123",
+//               dateOfBirth: "1990-01-01",
+//             };
+
+//     const errors = validateUser(user);
+   
+//     expect(errors).toHaveLength(1);
+//     expect(errors).toContainEqual("name can only contain letters and numbers, with no spaces");
+//   });
+
+//    test("should return an array with error messages if required fields are missing", () => {
+//     const user = {};
+
+//     const errors = validateUser(user);
+    
+//     expect(errors).toHaveLength(4);
+//     expect(errors).toContain("name is a required field");
+//     expect(errors).toContain("email is a required field");
+//     expect(errors).toContain("password is a required field");
+//     expect(errors).toContain("Date Of Birth is a required field");
+//   });
+
+//   test("should return an array with error messages if invalid fields are provided", () => {
+//     const user = {
+//       name: "Mary!!!@#$",
+//       email: "invalidemail",
+//       password: "weakpassword",
+//       dateOfBirth: "2022-02-30",
+//       invalidField: "invalid",
+//     };
+
+//     const errors = validateUser(user);
+    
+//     expect(errors).toHaveLength(5);
+//     expect(errors).toContain("name contains invalid characters in the username.");
+//     expect(errors).toContain("invalidemail is not a valid email address.");
+//     expect(errors).toContain("The password must be at least 8 characters long, including at least one digit, one uppercase letter, one lowercase letter, and one special character.");
+//     expect(errors).toContain("Date Of Birth is a required field");
+//     expect(errors).toContain("invalidField is not allowed.");
+//   });
+});
--- server/src/models/userModels.js
+++ server/src/models/userModels.js
@@ -4,48 +4,12 @@ import validateAllowedFields from "../util/validateAllowedFields.js";
 import { logInfo } from "../util/logging.js";
 
 const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    trim: true,
-    validate: {
-      validator: function(v) {
-        // Validate that the name does not contain special characters
-        return /^[a-zA-Z0-9 ]*$/.test(v);
-      },
-      message: props => `${props.value} contains invalid characters in the username.`
-    }
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    trim: true,
-    validate: {
-      validator: function(v) {
-        // Validate email format
-        return /\S+@\S+\.\S+/.test(v);
-      },
-      message: props => `${props.value} is not a valid email address.`
-    }
-  },
-  password: {
-    type: String,
-    required: true,
-    trim: true,
-    validate: {
-      validator: function(v) {
-        // Validate that the password contains at least one digit, one uppercase letter, one lowercase letter, and one special character
-        return /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*()_+}{"':;?/>.<,]).{8,}$/.test(v);
-      },
-      message: props => `The password must be at least 8 characters long, including at least one digit, one uppercase letter, one lowercase letter, and one special character.`
-    }
-  },
+  name: { type: String, required: true, trim: true },
+  email: { type: String, required: true, unique: true, trim: true },
+  password: { type: String, required: true, trim: true },
   dateOfBirth: { type: String, required: true, trim: true },
 });
 
-
-// Signup validation.
 export const validateUser = (
   userObject,
   requirePassword = true,
@@ -62,23 +26,29 @@ export const validateUser = (
     errorList.push(validatedKeysMessage);
   }
 
-  if (requireName && userObject.name == null) {
+  if (requireName && userObject.name == null || userObject.name == "") {
     errorList.push("name is a required field");
-    logInfo("user Create Validation failed: Name is required");
+    logInfo("user Create Validation failed: Name is required field");
+  }
+
+  if (requireName && !/^[a-zA-Z0-9]+$/.test(userObject.name)) {
+    errorList.push("name can only contain letters and numbers, with no spaces");
+    logInfo("user Create Validation failed: Name can only contain letters and numbers, with no spaces");
   }
 
   if (requireEmail && userObject.email == null) {
     errorList.push("email is a required field");
-    logInfo("User create Validation failed: Email is required");
+    logInfo("User create Validation failed: Email is required field");
   }
 
   if (requirePassword && userObject.password == null) {
     errorList.push("password is a required field");
-    logInfo("User create Validation failed: Password is required");
+    logInfo("User create Validation failed: Password is required field");
   }
+  
   if (requireDateOfBirth && userObject.dateOfBirth == null) {
     errorList.push("Date Of Birth is a required field");
-    logInfo("User create Validation failed: Date Of Birth is required");
+    logInfo("User create Validation failed: Date Of Birth is required field");
   }
 
   return errorList;
