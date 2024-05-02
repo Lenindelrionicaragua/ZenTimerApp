ca7732064ababb12dcd891dbd1e294e3fa3303a8
lenin del rio <lenindelrionicaragua@gmail.com>
2024-05-02 21:28:30 +0200

    test complete for loginController y sign up controller

--- server/src/__tests__/controllerTest/loginController.test.js
+++ server/src/__tests__/controllerTest/loginController.test.js
@@ -1,63 +1,101 @@
-import supertest from 'supertest';
+import supertest from "supertest";
 import app from "../../app.js";
 
 // tested with loginControllerMock.js because we are not testing the real DB and neither the bycryt dependenci only de loginController code
 const request = supertest(app);
 
-describe('loginController', () => {
-  test('Should fail if the request does not contain a valid email and password', async () => {
+describe("loginController", () => {
+  test("Should fail if the request does not contain a valid email and password", async () => {
     const userData = {
       email: "invalidUser@example.com",
-      password: "invalidPassword123"
+      password: "invalidPassword123",
     };
 
     const response = await request
-      .post('/api/test/log-in-mock')
+      .post("/api/test/log-in-mock")
       .send({ user: userData });
 
     expect(response.status).toBe(401);
     expect(response.body.success).toBe(false);
-    expect(response.body.msg).toBe('User not found');
+    expect(response.body.msg).toBe(
+      "No user was found associated with the provided email address. Please verify your email and try again or register if you are a new user."
+    );
   });
 
-  test('Should fail if the request contain an empty password', async () => {
+  test("Should fail if the request contain an empty password", async () => {
     const userData = {
       email: "jane@example.com",
-      password: ""
+      password: "",
     };
 
     const response = await request
-      .post('/api/test/log-in-mock')
+      .post("/api/test/log-in-mock")
       .send({ user: userData });
 
     expect(response.status).toBe(400);
     expect(response.body.success).toBe(false);
-    expect(response.body.error).toBe('BAD REQUEST: Email and password are required.');
+    expect(response.body.error).toBe(
+      "BAD REQUEST: Email and password are required."
+    );
   });
 
-  test('Should fail if the request does not contain a valid password', async () => {
+  test("Should fail if the request does not contain a valid password", async () => {
     const userData = {
       email: "jane@example.com",
-      password: "invalidPassword123"
+      password: "invalidPassword123",
     };
 
     const response = await request
-      .post('/api/test/log-in-mock')
+      .post("/api/test/log-in-mock")
       .send({ user: userData });
 
     expect(response.status).toBe(401);
     expect(response.body.success).toBe(false);
-    expect(response.body.msg).toBe('Incorrect password');
+    expect(response.body.msg).toBe(
+      "The password provided is incorrect. Please verify your password and try again."
+    );
   });
 
-  test('Should fail if the request is given with a empty object', async () => {
+  test("Should fail if the request is given with a empty object", async () => {
     const response = await request
-      .post('/api/test/log-in-mock')
+      .post("/api/test/log-in-mock")
       .send({ user: null });
 
     expect(response.status).toBe(400);
     expect(response.body.success).toBe(false);
-    expect(response.body.msg).toBe('Invalid request body');
+    expect(response.body.msg).toBe("Invalid request body");
+  });
+
+  test("Should fail if the request contain a invalid fields", async () => {
+    const userData = {
+      name: "Jane",
+      email: "jane@example.com",
+      password: "password123",
+    };
+
+    const response = await request
+      .post("/api/test/log-in-mock")
+      .send({ user: userData });
+
+    expect(response.status).toBe(400);
+    expect(response.body.success).toBe(false);
+    expect(response.body.error).toBe(
+      "BAD REQUEST: Invalid fields present in the request."
+    );
+  });
+
+  test("Should pass if the request contain a valid password and email", async () => {
+    const userData = {
+      email: "jane@example.com",
+      password: "password123",
+    };
+
+    const response = await request
+      .post("/api/test/log-in-mock")
+      .send({ user: userData });
+
+    expect(response.status).toBe(200);
+    expect(response.body.success).toBe(true);
+    expect(response.body.msg).toBe("Login successful");
   });
 });
-
--- server/src/controllers/AuthControllers/loginController.js
+++ server/src/controllers/AuthControllers/loginController.js
@@ -5,19 +5,25 @@ import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 
 export const login = async (req, res) => {
- // Check if the request body contains user information
- if (!req.body || typeof req.body !== "object" || !req.body.user) {
-  return res.status(400).json({ success: false, msg: "Invalid request body" }); // Update error message here
- } 
+  // Check if the request body contains user information
+  if (!req.body || typeof req.body !== "object" || !req.body.user) {
+    return res
+      .status(400)
+      .json({ success: false, msg: "Invalid request body" }); // Update error message here
+  }
 
-  const { user } = req.body;
-   
+  const { email, password, ...additionalFields } = req.body.user;
+
   // Validation Errors
 
   try {
     const errors = [];
-    if (!user.email || !user.password) {
+    if (!email || !password) {
       errors.push("Email and password are required.");
+    }
+
+    if (Object.keys(additionalFields).length > 0) {
+      errors.push("Invalid fields present in the request.");
     }
 
     if (errors.length > 0) {
@@ -25,17 +31,17 @@ export const login = async (req, res) => {
       res.status(400).json({ success: false, error: errorMessage });
       return;
     }
-    
-    // Continue with the login process if there are no validation errors
-    const userFound = await User.findOne({ email: user.email });
 
-    logInfo(`User does not exist`);
+    // Continue with the login process if there are no validation errors
+    const userFound = await User.findOne({ email: email });
+
+    logInfo("User does not exist");
 
     if (userFound) {
       logInfo(`User found: ${JSON.stringify(userFound)}`);
 
       const isPasswordValid = await bcrypt.compare(
-        user.password,
+        password,
         userFound.password
       );
 
@@ -47,7 +53,7 @@ export const login = async (req, res) => {
         // Create jwt token
         const token = jwt.sign(
           { userId: userFound._id.toString() },
-         process.env.JWT_SECRET 
+          process.env.JWT_SECRET
         );
 
         // Save token in cookie
@@ -57,7 +63,6 @@ export const login = async (req, res) => {
           sameSite: "lax",
         });
 
-       
         res.status(200).json({
           success: true,
           msg: "Login successful",
@@ -69,12 +74,27 @@ export const login = async (req, res) => {
           },
         });
       } else {
-        res.status(401).json({ success: false, msg: "Incorrect password" });
+        res
+          .status(401)
+          .json({
+            success: false,
+            msg: "The password provided is incorrect. Please verify your password and try again.",
+          });
       }
     } else {
-      res.status(401).json({ success: false, msg: "User not found" });
+      res
+        .status(401)
+        .json({
+          success: false,
+          msg: "No user was found associated with the provided email address. Please verify your email and try again or register if you are a new user.",
+        });
     }
   } catch (error) {
-    res.status(500).json({ success: false, msg: "Internal server error" });
+    res
+      .status(500)
+      .json({
+        success: false,
+        msg: "An internal server error occurred. Please try again later or contact technical support if the issue persists.",
+      });
   }
 };
